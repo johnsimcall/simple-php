fa4db5af0522fee0fc85db0f04710f61bdb95eec
John Call <johnsimcall@gmail.com>
2022-12-13 10:55:07 -0700

    Update index.php

--- index.php
+++ index.php
@@ -8,11 +8,11 @@
        echo 'You requested the contents of: <b>'; echo $_SERVER['HTTP_HOST']; echo '</b><br>';
 //       echo 'The Pod that answered is: <b>'; echo $_ENV['HOSTNAME']; echo '</b> with an internal IP of <b>'; echo $_SERVER['SERVER_ADDR']; echo '</b><br>';
        echo 'The Pod that responded is: <b>'; echo $_ENV['OCP_POD_NAME']; echo '</b> with an internal IP of <b>'; echo $_ENV['OCP_POD_IP']; echo '</b><br>';
-       echo 'That Pod is running on Node: '; echo $_ENV['OCP_NODE_NAME']; echo '</b> who\'s primary IP is <b>'; echo $_ENV['OCP_NODE_IP']; echo '</b><br>';
+       echo 'That Pod is running on Node: <b>'; echo $_ENV['OCP_NODE_NAME']; echo '</b> who\'s primary IP is <b>'; echo $_ENV['OCP_NODE_IP']; echo '</b><br>';
        echo 'The clusterIP subnet/gateway assigned to that node is: <b>'; echo $_SERVER['REMOTE_ADDR']; echo '</b><br><br>';
   
        if (empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-         echo 'Direct connection, no proxy or HTTP_X_FORWARD_FOR not set.<br>You appear to be: <b>'; echo $_SERVER['REMOTE_ADDR']; echo '</b><br>';
+         echo 'Direct connection, no proxy, or HTTP_X_FORWARD_FOR not set.<br>';
        } else {
          echo 'The proxy says you are: <b>'; echo $_SERVER['HTTP_X_FORWARDED_FOR']; echo '</b><br>';
        }
