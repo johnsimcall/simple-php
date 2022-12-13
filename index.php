bf7eb0d85c98fb1478279ab463fae44a9afadd02
John Call <johnsimcall@gmail.com>
2022-12-12 18:23:38 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
-<?php
-$everything = get_defined_vars();
-ksort($everything);
-
-?>
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ <?php
+  $everything = get_defined_vars();
+  ksort($everything);
+ ?>
+ </body>
+</html>
