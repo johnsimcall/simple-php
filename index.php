5302ab365c0ae8a7e373f714170e6eb1922d6f0d
John Call <johnsimcall@gmail.com>
2022-12-12 20:49:49 -0700

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,26 @@
  <head>
   <title>PHP Test</title>
  </head>
+
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+ <?php echo '<h1>Hello World!</h1>'; 
+       echo 'You requested the contents of: <b>'; echo $_SERVER['SERVER_NAME']; echo '</b><br>';
+       echo 'My name is: <b>'; echo $_ENV['HOSTNAME']; echo '</b> at <b>'; echo $_SERVER['SERVER_ADDR']; echo '</b><br>';
+ 
+       if (empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+         echo 'Your <u>proxy</u> reports that your name is: <b>'; echo $_SERVER['HTTP_X_FORWARDED_FOR']; echo "</b><br>";
+         echo 'REMOTE_ADDR is: <b>'; echo $_SERVER['REMOTE_ADDR']; echo "</b><br>;
+       } else {
+         echo 'No proxy, or HTTP_X_FORWARD_FOR not set. You appear to be: <b>'; echo $_SERVER['REMOTE_ADDR']; echo "</b><br>;
+       }
+ ?>
+
  <?php
   $everything = get_defined_vars();
   ksort($everything);
+  echo '<pre>'; print_r($everything); echo '</pre>';
  ?>
+
+ <?php phpinfo(); ?>
  </body>
 </html>
